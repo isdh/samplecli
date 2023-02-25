0ed0b4afd8df96555448623eef5e5b38678f1188
isdh <hideorseekyes+github@gmail.com>
2023-02-26 00:23:54 +0900

    refactor: recode by derive macro feature

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,36 @@
-use clap::{Command, Arg, ArgAction};
+use clap::{Parser, command};
+
+#[derive(Parser, Debug)]
+#[command(
+    name = "My RPN program",
+    version = "1.0.1",
+    author = "isdh",
+    about = "Super awesome sample RPN calculator",
+    help_template = "\
+////////////////////////////////////
+{name} {version} created by {author}.
+this is {about}.
+///////////////////////////////////"
+)]
+
+struct Opts {
+    /// sets the level of verbosity
+    #[clap(short,long)]
+    verbose: bool,
+
+    /// Formulas written in RPN
+    #[clap(name = "FILE")]
+    formula_file: Option<String>
+}
 
 fn main() {
-    let matches = Command::new("My RPN program")
-    // コマンドラインのバージョン
-    .version("1.0.0")
-    // 作者名
-    .author("isdh")
-    // 説明
-    .about("Super awesome sample RPN calculator")
-    // デフォルトのヘルプでは作者名やコマンド名は表示されなくなった
-    // Command::help_templateを使用すると表示できる
-    .help_template("\
-{name} v{version} created by {author}
-    ")
-    // コマンドライン引数定義
-    .arg(
-        Arg::new("formula_file")
-        // .aboutは@deprecated
-        .help("Formulas written in RPN")
-        .value_name("FILE")
-        .index(1)
-        .required(false),
-    )
-    // コマンドライン引数定義
-    .arg(
-        // -vで表示するような内容
-        Arg::new("verbose")
-        .help("Sets the level of verbosity")
-        .short('v')
-        .long("verbose")
-        .required(false)
-        // v4からflagで管理するのを推奨っぽい
-        // https://github.com/clap-rs/clap/blob/master/CHANGELOG.md#migrating
-        .action(ArgAction::SetTrue),
-    )
-    .get_matches();
+    let opts = Opts::parse();
 
     // 以下はget_one/get_manyに変わった
     // match matches.value_of("formula_file") {
-    match matches.get_one::<String>("formula_file") {
+    match opts.formula_file {
         Some(file) => println!("File speecified: {}", file),
         None => println!("No file specified."),
     }
-    // 以下はget_flagに変わった
-    // let verbose = matches.is_present("verbose");
-    let verbose = matches.get_flag("verbose");
-    println!("Is verbosity specified?: {} ", verbose );
+    println!("Is verbosity specified?: {} ", opts.verbose );
 }
